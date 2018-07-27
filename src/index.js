74365c9b3e78de062ba6987cfcad1be2fa0490e6
Tianhui Cao <tianhui.cao@appfolio.com>
2018-07-26 20:57:31 -0700

    sc - Fixes naming error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './app.js';
+import App from './App.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
